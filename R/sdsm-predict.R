1268088f0c17b4718de19d22de455e833fc255a9
leerichardson <leerichardson2013@gmail.com>
2015-06-30 14:11:44 -0600

    fixed the conditional predictions

--- R/sdsm-predict.R
+++ R/sdsm-predict.R
@@ -38,6 +38,10 @@ generate_weather <- function(models, new_dataframe, uncertainty = "ensemble",
         new_dataframe <- add_month(new_dataframe)
         for (i in names(models)) {
             sub_month <- subset(new_dataframe, month == which(names(models) == i))
+
+            # If the type of model is conditional, instead of unconditional,
+            # obtain the predictions from the predict_conditional
+            # function
             if (length(models[[i]]) == 2) {
                 sub_preds <- predict_conditional(sub_month, models[[i]]$step1,
                                                  models[[i]]$step2)
@@ -54,18 +58,36 @@ generate_weather <- function(models, new_dataframe, uncertainty = "ensemble",
         new_dataframe <- add_season(new_dataframe, "month")
         for (i in names(models)) {
             sub_season <- subset(new_dataframe, season == i)
-            sub_preds <- predict.lm(models[[i]], newdata = sub_season)
+
+            # If the type of model is conditional, instead of unconditional,
+            # obtain the predictions from the predict_conditional
+            # function
+            if (length(models[[i]]) == 2) {
+                sub_preds <- predict_conditional(sub_season, models[[i]]$step1,
+                                                 models[[i]]$step2)
+            } else {
+                sub_preds <- predict.lm(models[[i]], newdata = sub_month)
+            }
+
             preds <- c(preds, sub_preds)
             dates <- c(dates, sub_season[, date_column_index])
         }
     } else if (num_mods == 1) {
-        preds <- predict.lm(models[[1]], newdata = new_dataframe)
+
+        # If the type of model is conditional, instead of unconditional,
+        # obtain the predictions from the predict_conditional
+        # function
+        if (length(models[[1]]) == 2) {
+            preds <- predict_conditional(new_dataframe, models[[1]]$step1,
+                                             models[[1]]$step2)
+        } else {
+            preds <- predict.lm(models[[1]], newdata = new_dataframe)
+        }
+
         dates <- c(dates, new_dataframe[, date_column_index])
     } else {
         stop("List must contain 1, 4, or 12 models!")
     }
-
-    browser()
 
     # Return a dataframe ordered by the datesused
     final_df <- data.frame(dates = dates, predictions = preds)
@@ -76,6 +98,7 @@ generate_weather <- function(models, new_dataframe, uncertainty = "ensemble",
     if (uncertainty == "ensemble" & num_ensembles == 1) {
         return(final_df[order_by_date,])
     } else if (uncertainty == "ensemble" & num_ensembles > 1) {
+
         average_sigma <- mean(unlist(lapply(models,
                                         function(x) summary(x)$sigma)))
         ensembles <- generate_ensembles(final_df$predictions,
@@ -89,7 +112,8 @@ generate_weather <- function(models, new_dataframe, uncertainty = "ensemble",
 
 # Function to generate ensembles for various predictions made with
 # the generate_weather function
-generate_ensembles <- function(predictions, num_ensembles, sigma){
+generate_ensembles <- function(predictions, num_ensembles, sigma,
+                               conditional = FALSE) {
     # Get the number of predicted values from the input, and use this
     # along with the number of ensembles to create a white noise
     # matrix. The white noise matrix uses the average standard
@@ -125,7 +149,7 @@ predict_conditional <- function(dataframe, lm1, lm2) {
     cond_preds <- vector(mode = "numeric", length = 0)
     for (i in 1:nrow(dataframe)) {
         prediction <- predict_row(dataframe[i, ], lm1, lm2)
-        cond_preds <- c(cond_preds, predictio
+        cond_preds <- c(cond_preds, prediction)
     }
     return(cond_preds)
 }
--- tests/testthat/test-predictions.R
+++ tests/testthat/test-predictions.R
@@ -1,12 +1,43 @@
 library(stringr)
 context("Predictions")
 
-test_that("generate weather returns the correct size dataframe", {
-    blogsville_mod <- calibrate_model(blogsville, y = "tmax", model_type = "annual")
-    annual_preds <- generate_weather(blogsville_mod,
+# Unconditional Models -----------------
+test_that("generate weather returns the correct size dataframe for unconditional models", {
+    # Set up the data to use
+    annual_mod <- calibrate_model(blogsville, y = "tmax", model_type = "annual")
+    annual_preds <- generate_weather(annual_mod,
                                      new_dataframe = blogsville, uncertainty = "ensemble",
-                                     num_ensembles = 20 )
+                                     num_ensembles = 20)
 
+    # Test on this set-up data
     expect_output(str(annual_preds), "10957 obs. of  22 variables:", all = FALSE)
     }
 )
+
+
+# Conditional Models -------------------
+# Set up the data to use for the tests
+test_that("generate weather returns the correct size dataframe for unconditional models", {
+        # Monthly Conditional Predictions
+        month_cond_mod <- calibrate_model(dataframe = blogsville[, -7],
+                                y = "pcrp", process = "conditional", model_type = "monthly")
+        month_cond_preds <- generate_weather(models = month_cond_mod, new_dataframe = blogsville)
+
+        # Seasonal Conditional Predictions
+        season_cond_mod <- calibrate_model(dataframe = blogsville[, -7],
+                                y = "pcrp", process = "conditional", model_type = "seasonal")
+        season_cond_preds <- generate_weather(models = season_cond_mod, new_dataframe = blogsville)
+
+        # Annual Conditional Predictions
+        annual_cond_mod <- calibrate_model(dataframe = blogsville[, -7],
+                                           y = "pcrp", process = "conditional", model_type = "annual")
+        annual_cond_preds <- generate_weather(models = annual_cond_mod, new_dataframe = blogsville)
+
+        # Test using the set-up data. Make sure that the three different types of
+        # models are returning the correct data structure.
+        expect_output(str(month_cond_preds), "10957 obs. of  2 variables:", all = FALSE)
+        expect_output(str(season_cond_preds), "10957 obs. of  2 variables:", all = FALSE)
+        expect_output(str(annual_cond_preds), "10957 obs. of  2 variables:", all = FALSE)
+    }
+)
+
